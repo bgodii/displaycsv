d4c8ccab0f6f356ba4582ef5b41b5d090accb0ab
Renan Dantas <reehdantascarvalho@gmail.com>
2025-11-13 22:49:51 +0000

    cp_sakai

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-from flask import Flask, request, render_template
 import pandas as pd
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    df = pd.read_csv("ai_job_trends_dataset.csv", sep=",")
+    return render_template(
+        "display.html", tables=[df.to_html(classes="data")], titles=df.columns.values
+    )
 
-@app.route('/display', methods=['POST'])
-def display_file():
-    file = request.files['file']
-    if not file:
-        return "No file"
 
-    # Tenta ler o arquivo com diferentes codificações
-    try:
-        df = pd.read_csv(file, delimiter=";")
-    except UnicodeDecodeError:
-        file.seek(0)  # Reseta o ponteiro do arquivo
-        try:
-            df = pd.read_csv(file, encoding='latin1', delimiter=";")
-        except UnicodeDecodeError:
-            file.seek(0)  # Reseta o ponteiro do arquivo
-            df = pd.read_csv(file, encoding='ISO-8859-1', delimiter=";")
-
-    # Renderiza o template com os dados do arquivo CSV
-    return render_template('display.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
